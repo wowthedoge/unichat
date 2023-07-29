b4a79e8887c7454328cbeed2214a73a8ade2dfc4
ktan088 <ktan0088@student.monash.edu>
2023-07-29 18:41:15 +0800

    added join room

--- public/index.js
+++ public/index.js
@@ -3,23 +3,14 @@
 
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyBQ0wZTA14UCOJaHJ8DcHPHGPr3BdfcT6w",
-
     authDomain: "unichat-2a034.firebaseapp.com",
-
     databaseURL: "https://unichat-2a034-default-rtdb.asia-southeast1.firebasedatabase.app",
-
     projectId: "unichat-2a034",
-
     storageBucket: "unichat-2a034.appspot.com",
-
     messagingSenderId: "291619298133",
-
     appId: "1:291619298133:web:8d488a239dabad789c6ddc",
-
     measurementId: "G-DXZSHXMQ1C"
-
 };
 
 
@@ -30,11 +21,48 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 const db = firebase.database();
 
-console.log("db", db)
+const username = prompt("Your username: ");
+
+const roomForm = document.getElementById('room-form')
+roomForm.addEventListener('submit', joinRoom)
+
+let roomId;
+let fetchChat;
 
 
-const username = prompt("Please Tell Us Your Name");
-console.log("username:", username)
+function joinRoom(e) {
+    console.log("joinRoom")
+    e.preventDefault()
+    roomId = document.getElementById('room-input').value
+    document.getElementById('message-form').style.display = 'block'
+    document.getElementById('messages').style.display = 'block'
+    document.getElementById('back-btn').style.display = 'block'
+    document.getElementById('room-form').style.display = 'none'
+    document.getElementById('title').innerText = roomId
+    document.getElementById("messages").innerHTML +=
+        `<li> Welcome ${username} </li>`;   
+    fetchChat = db.ref(roomId + "/")
+    fetchChat.on("child_added", function (snapshot) {
+        const messages = snapshot.val();
+        const message = `<li class=${username === messages.username ? "sent" : "receive"
+            }><span>${messages.username}: </span>${messages.message}</li>`;
+        // append the message on the page
+        document.getElementById("messages").innerHTML += message;
+    });
+}
+
+const backButton = document.getElementById('back-btn')
+backButton.addEventListener('click', backClicked)
+
+function backClicked() {
+    fetchChat = undefined
+    roomId = undefined
+    document.getElementById('message-form').style.display = 'none'
+    document.getElementById('messages').style.display = 'none'
+    document.getElementById('back-btn').style.display = 'none'
+    document.getElementById('room-form').style.display = 'block'
+    document.getElementById('title').innerText = 'Firebase RealTime Chat'
+}
 
 const messageForm = document.getElementById('message-form')
 messageForm.addEventListener('submit', sendMessage)
@@ -55,21 +83,11 @@ function sendMessage(e) {
         .getElementById("messages")
         .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 
-    
-    console.log("db, db.ref", db, db.ref)
     // create db collection and send in the data
-    db.ref("messages/" + timestamp).set({
+    db.ref(roomId + "/" + timestamp).set({
         username,
         message,
     });
 }
 
-const fetchChat = db.ref("messages/");
 
-fetchChat.on("child_added", function (snapshot) {
-    const messages = snapshot.val();
-    const message = `<li class=${username === messages.username ? "sent" : "receive"
-        }><span>${messages.username}: </span>${messages.message}</li>`;
-    // append the message on the page
-    document.getElementById("messages").innerHTML += message;
-});
